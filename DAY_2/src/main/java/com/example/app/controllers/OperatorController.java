db687fe91ed25018ae9b272025c8ec9fef8b6383
leonx04 <jungnguyen2108@gmail.com>
2024-07-02 17:59:25 +0700

    feat: Add new controllers and templates

--- DAY_2/src/main/java/com/example/app/controllers/OperatorController.java
+++ DAY_2/src/main/java/com/example/app/controllers/OperatorController.java
@@ -1,2 +1,16 @@
-package com.example.app.controllers;public class OperatorController {
+package com.example.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OperatorController {
+
+    @RequestMapping("/operator")
+    public String index(Model model) {
+        model.addAttribute("x", 5);
+        model.addAttribute("y", 3);
+        return "operator/index";
+    }
 }
--- DAY_2/src/main/java/com/example/app/controllers/SinhViens2Controller.java
+++ DAY_2/src/main/java/com/example/app/controllers/SinhViens2Controller.java
@@ -1,2 +1,30 @@
-package com.example.app.controllers;public class SinhViens2Controller {
+package com.example.app.controllers;
+
+import com.example.app.beans.SinhVien;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class SinhViens2Controller {
+    @RequestMapping("/students2/list")
+    public String list(Model model, @RequestParam("index") Optional<Integer> index) throws IOException {
+        File file = new ClassPathResource("/static/SinhViens2.json").getFile();
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<SinhVien>> type = new TypeReference<List<SinhVien>>() {};
+        List<SinhVien> list = objectMapper.readValue(file, type);
+
+        model.addAttribute("sv", list.get(index.orElse(0)));
+        model.addAttribute("list", list);
+        return "students2/list";
+    }
 }
