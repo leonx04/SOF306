8c7e8e6d4647dc03a891a77686b9a01c08415f4d
leonx04 <jungnguyen2108@gmail.com>
2024-07-12 16:52:19 +0700

    feat: Add RestTemplateApplication configuration to launch.json
    This commit adds the configuration for launching the `RestTemplateApplication` in the `launch.json` file. It includes the necessary properties such as the main class, project name, and request type. This configuration enables easier debugging and running of the application using RestTemplate.

--- Day4/RestTemplate/src/main/java/com/example/resttemplate/controllers/StudentController.java
+++ Day4/RestTemplate/src/main/java/com/example/resttemplate/controllers/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,40 +18,36 @@ public class StudentController {
     StudentDAO studentDAO;
 
     @GetMapping("/index")
-    public String index(Model model){
-        Student student = new Student();
-        model.addAttribute("form", student);
-        StudentMap studentMap = studentDAO.findAll();
-        model.addAttribute("list", studentMap);
+    public String index(Model model) {
+        model.addAttribute("form", new Student());
+        model.addAttribute("list", studentDAO.findAll());
         return "student/list";
     }
 
     @PostMapping("/create")
-    public String create(Student student){
+    public String create(Student student) {
         studentDAO.create(student);
         return "redirect:/students/index";
     }
 
     @GetMapping("/edit{key}")
-    public String edit(String key, Model model){
-        Student student = studentDAO.findByKey(key);
-        model.addAttribute("form", student);
-        StudentMap studentMap = studentDAO.findAll();
-        model.addAttribute("list", studentMap);
+    public String edit(@PathVariable String key, Model model) {
+        model.addAttribute("form", studentDAO.findByKey(key));
+        model.addAttribute("key", key);
+        model.addAttribute("list", studentDAO.findAll());
         return "student/list";
     }
 
     @PostMapping("/update{key}")
-    public String update(String key, Student student){
+    public String update(@PathVariable String key, Student student) {
         studentDAO.update(key, student);
         return "redirect:/students/index";
     }
 
     @GetMapping("/delete{key}")
-    public String delete(String key){
+    public String delete(@PathVariable String key) {
         studentDAO.delete(key);
         return "redirect:/students/index";
     }
-
-
+    
 }
