881a6b3737b9d56ebb9b05dbebc8ecc1688920ca
leonx04 <jungnguyen2108@gmail.com>
2024-07-13 16:49:23 +0700

    feat: Add Student entity class

---
+++ Day5/RestAPICustom/src/main/java/com/example/app/RestApiCustomApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiCustomApplication.class, args);
+    }
+
+}
---
+++ Day5/RestAPICustom/src/main/java/com/example/app/entity/Student.java
@@ -0,0 +1,26 @@
+package com.example.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    public String email;
+
+    public String fullname;
+
+    public Double marks;
+
+    public Boolean gender;
+
+    public String country;
+}
